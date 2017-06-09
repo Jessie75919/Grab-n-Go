aade3afa9ab2cd887f171d3193cc46f94f90ae6b
Jessie75919 <jessie75919@gmail.com>
2017-06-09 09:52:18 +0800

    remove a line of comment

--- src/_00_Database_ver01/InitData_Main.java
+++ src/_00_Database_ver01/InitData_Main.java
@@ -3,7 +3,6 @@ package _00_Database_ver01;
 public class InitData_Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		TableDAO dao = new TableDAO();
 		dao.insertMember();
 

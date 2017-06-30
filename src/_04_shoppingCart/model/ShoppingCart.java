696b8c2e5abe9e1e2c76708e1d17e4898f8bccb0
Jessie75919 <jessie75919@gmail.com>
2017-06-30 21:08:09 +0800

    完成同一個品項卻不同備註分類數量的功能

--- src/_04_shoppingCart/model/ShoppingCart.java
+++ src/_04_shoppingCart/model/ShoppingCart.java
@@ -22,26 +22,60 @@ public class ShoppingCart {
 	public void addToCart(int prod_Id,OrderItemBean odb){
 		
 		if(shoppingCart.get(prod_Id)==null){
-			List<OrderItemBean> al = new ArrayList<>();
-			al.add(odb);
-			shoppingCart.put(prod_Id, al);
+			List<OrderItemBean> newList = new ArrayList<>();
+			newList.add(odb);
+			shoppingCart.put(prod_Id, newList);
 		}else{
-			List<OrderItemBean> al = shoppingCart.get(prod_Id);
+			List<OrderItemBean> oldList = shoppingCart.get(prod_Id);
 			 /*if the amount of pro_id is the only one*/
-			if(al.size()==0){
-				List<OrderItemBean> a = new ArrayList<>();
-				a.add(odb);
-				shoppingCart.put(prod_Id, a);
-			}else{
-				/*if the note is the same,put them together */ 
-				for(OrderItemBean oldOdb : al){
-					if(oldOdb.getItem_note().equals(odb.getItem_note())){
-						oldOdb.setItem_amount(oldOdb.getItem_amount()+odb.getItem_amount());
-					}
+//			if(al.size()==0){
+//				List<OrderItemBean> a = new ArrayList<>();
+//				a.add(odb);
+//				shoppingCart.put(prod_Id, a);
+//			}
+//			else{
+				/*if the note is the same,put them together */
+				int[] result = isExist(odb,oldList);
+				// result[0] : 0 == 不存在 ; 1== 已存在
+				if(result[0]==0){
+					oldList.add(odb);
+					shoppingCart.put(prod_Id, oldList);
+				}else{
+					oldList.get(result[1]).setItem_amount(oldList.get(result[1]).getItem_amount()+odb.getItem_amount());
+					
 				}
-			}
+			
+//				for(OrderItemBean oldOdb : oldList){
+//					if(oldOdb.getItem_note().equals(odb.getItem_note())){
+//						oldOdb.setItem_amount(oldOdb.getItem_amount()+odb.getItem_amount());
+//					}else{
+//						break;
+//					}
+//				}
+//				oldList.add(odb);
+//				shoppingCart.put(prod_Id, oldList);
+//			}
 		}
 	}
+	
+	public int[] isExist(OrderItemBean newOid,List<OrderItemBean> oldList){
+		int[] result = new int[2];
+		
+		for(int i=0;i<oldList.size();i++){
+			if(oldList.get(i).getItem_note().equals(newOid.getItem_note())){
+				result[0] = 1;
+				result[1] = i;
+				break;
+			}else{
+				result[0] = 0;
+			}
+		}
+		
+		return result;
+	}
+	
+	
+	
 	/*
 	public boolean modifyAmount(int prod_Id,OrderItemBean odb){
 		boolean result = false;

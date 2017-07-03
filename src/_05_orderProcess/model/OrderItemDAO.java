b20ead39b38fab209cd5b531107d5b1a9ec2d5ed
PC219-25\NTUT <icypisces318@gmail.com>
2017-07-03 10:40:30 +0800

    App 取得收入分析相關訂單資料 Eclipse部分OK

--- src/_05_orderProcess/model/OrderItemDAO.java
+++ src/_05_orderProcess/model/OrderItemDAO.java
@@ -82,6 +82,7 @@ public class OrderItemDAO {
 	
 	public Collection<OrderItemBean> getOrdersItemDataForApp(String rest_name) {
 		Collection<OrderItemBean> coll = new ArrayList<>();
+		OrderItemBean oib = null;
 		String sql = "SELECT a.prod_id, a.item_name, a.item_price, a.item_amount "
 				+ " FROM order01 b JOIN order_item a ON a.ord_id = b.ord_id "
 				+ " JOIN restaurant r ON b.rest_id = r.rest_id "
@@ -93,11 +94,12 @@ public class OrderItemDAO {
 			stmt.setString(1, rest_name);
 			ResultSet rs = stmt.executeQuery();
 			while (rs.next()) {
-				OrderItemBean oib = new OrderItemBean();
-				oib.setProd_id(rs.getInt("prod_id"));
-				oib.setItem_name(rs.getString("item_name"));
-				oib.setItem_price(rs.getInt("item_price"));
-				oib.setItem_amount(rs.getInt("item_amount"));
+				int prod_id = rs.getInt("prod_id");
+				String item_name = rs.getString("item_name");
+				int item_price = rs.getInt("item_price");
+				int item_amount = rs.getInt("item_amount");
+				
+				oib = new OrderItemBean(prod_id, item_name, item_price, item_amount);
 				coll.add(oib);
 			}
 		} catch (Exception ex) {
--- src/_23_App_StoreOrderAnalysis/controller/AppRevenueServlet.java
+++ src/_23_App_StoreOrderAnalysis/controller/AppRevenueServlet.java
@@ -3,13 +3,8 @@ package _23_App_StoreOrderAnalysis.controller;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collection;
-import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,6 +16,8 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 import _05_orderProcess.model.OrderBean;
+import _05_orderProcess.model.OrderItemBean;
+import _05_orderProcess.model.OrderItemDAO;
 
 @SuppressWarnings("serial")
 @WebServlet("/AppRevenueServlet")
@@ -44,33 +41,18 @@ public class AppRevenueServlet extends HttpServlet {
 		
 		//判斷送來的請求為要求revernueDate或是orderInformation
 		String param = jsonObject.get("param").getAsString();
-		System.out.println("param = " + param);
-		Collection<OrderBean> coll = new ArrayList<>();
-		if (param.equals("revernueDate")) {
+		Collection<OrderItemBean> coll = new ArrayList<>();
+		if (param.equals("getOrderData")) {
 			String rest_name = jsonObject.get("rest_name").getAsString();
-			//取得現在日期時間
-			Calendar now_c = Calendar.getInstance(); 
-			Date now = now_c.getTime();
-			System.out.println("now = " + now);
-			//取得固定月份前日期(無時分秒)
-			TimeChange timeChange = new TimeChange();
-			Date threeMonthsAgo = timeChange.GetTimeBeforeAndOnlyDate(now_c, -3);
-			System.out.println("threeMonthsAgo = " + threeMonthsAgo);
-			//呼叫OrderDAO方法取得固定時間內的訂單日期
-			
-			System.out.println("jsonIn = " + jsonIn);
-			
-//			coll = getOrdersDate(rest_name,,);
-			System.out.println("coll = " + coll);
-			
-			//將訂單日期送回App
-			PrintWriter out = response.getWriter();
-			out.println(gson.toJson(coll));
-			out.close();
-		} else if (param.equals("orderInformation")){
-			
+			OrderItemDAO dao = new OrderItemDAO();
+			coll = dao.getOrdersItemDataForApp(rest_name);
 		}
 		
+		//將訂單資料送回App
+		PrintWriter out = response.getWriter();
+		out.println(gson.toJson(coll));
+		System.out.println("gson.toJson(coll) = " + gson.toJson(coll));
+		out.close();
 	}
 
 }

f8cd1bc7d549e5b2c9d1a2a3fcb0312490496e02
LAPTOP-C1O370F4\icypi <icypisces318@gmail.com>
2017-07-08 23:11:42 +0800

    App 當日訂單OrderDAO修正

--- src/_05_orderProcess/model/OrderDAO.java
+++ src/_05_orderProcess/model/OrderDAO.java
@@ -613,27 +613,26 @@ public class OrderDAO {
 		String sql1 = " SELECT ord_id, m_pickupname, ord_time, ord_totalPrice, "
 					+ " ord_tel, ord_status, ord_pickuptime , ord_evalued "
 					+ " FROM order01 "
-					+ " WHERE rest_id = ? AND DATE_FORMAT(ord_pickuptime, '%Y-%c-%e') "
-					+ " <= AND DATE_FORMAT(NOW(), '%Y-%c-%e') "
+					+ " WHERE rest_id = ? AND ord_pickuptime >= CURDATE() "
 					+ " AND ord_status IN (?) ";
 		String sql2 = "";
 		String sql3 = " ORDER BY ord_pickuptime DESC ";
-		if(mPickupName.length() != 0){
-			sql2 = " AND m_pickupname LIKE ? ";
-		}
+//		if(mPickupName.length() != 0){
+//			sql2 = " AND m_pickupname LIKE ? ";
+//		}
 		String sql = sql1 + sql2 + sql3;
 		try (
 			Connection con = ds.getConnection();
 			PreparedStatement stmt = con.prepareStatement(sql);	
 		){
-			if(mPickupName.length() == 0){
+//			if(mPickupName.length() == 0){
 				stmt.setInt(1, restId);
 				stmt.setString(2, ord_status);
-			} else{
-				stmt.setInt(1, restId);
-				stmt.setString(2, ord_status);
-				stmt.setString(3, "%" + mPickupName + "%");
-			}
+//			} else{
+//				stmt.setInt(1, restId);
+//				stmt.setString(2, ord_status);
+//				stmt.setString(3, "%" + mPickupName + "%");
+//			}
 			ResultSet rs = stmt.executeQuery();
 			while(rs.next()){
 				OrderItemDAO dao = new OrderItemDAO();
